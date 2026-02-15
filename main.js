e2e006521fd3662f65431150373c3a7a90fa70ec
Michael Analitis <m.analitis3@gmail.com>
2026-02-14 18:10:53 -0600

    Add checkboxes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function roll() {
     for (let i = 1; i <= 5; i++) {
-        let rand = Math.floor(Math.random()*6) + 1
-        document.getElementById(`result${i}`).setAttribute("value",rand)
+        if (!document.getElementById(`keep${i}`).checked) {
+            let rand = Math.floor(Math.random()*6) + 1
+            document.getElementById(`result${i}`).setAttribute("value",rand)
+        }
     }
 }
