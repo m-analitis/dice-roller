8aba27262fb851fd6dfc874adc87d332b6496c09
Michael Analitis <m.analitis3@gmail.com>
2026-03-01 19:45:16 -0600

    Implement wakeup function

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const url = "https://dice-roller-nodejs-f7ayd2a0b0f6eng4.centralus-01.azurewebsites.net/"
 
+function load() {
+	wakeup()
+	roll()
+}
+
+// Pings the Node.js server to make sure it is running
+async function wakeup() {
+	const fetchString = url + "/api/ping"
+	const response = await fetch(fetchString)
+	const responseText = await response.text()
+	console.log(responseText)
+}
+
+// Requests dice rolls from the Node.js server
 async function roll() {
 	let unchecked = getUnchecked()
 	let num = unchecked.length
@@ -11,6 +25,7 @@ async function roll() {
 	}
 }
 
+// Gets the IDs of all dice to reroll
 function getUnchecked() {
 	let unchecked = []
     for (let i = 1; i <= 5; i++) {
@@ -20,4 +35,3 @@ function getUnchecked() {
     }
 	return unchecked
 }
-//document.getElementById(`result${i}`).setAttribute("value",rand)
